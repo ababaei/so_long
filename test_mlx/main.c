a79ee222b685a6299cff942e0dc0e8dcdf6108ff
ababaei <ababaei@student.42.fr>
2021-10-08 00:17:02 +0200

    DONE: set up MLX in test_mlx/ TODO: Start using it in So_long

--- test_mlx/main.c
+++ test_mlx/main.c
@@ -1,11 +1,109 @@
 #include <stdlib.h>
-#include <mlx.h>
+#include <stdio.h>
 
-int main(void)
+#include <mlx.h>
+#include <X11/keysym.h>
+#include <X11/X.h>
+
+# define WINDOW_WIDTH 600 
+# define WINDOW_HEIGHT 300
+
+# define RED_PIXEL 0xFF0000
+# define WHITE_PIXEL 0xFFFFFF
+
+typedef struct s_img
+{
+	void *mlx_img;
+	char *addr;
+	int bpp;
+	int line_len;
+	int endian;
+}	t_img;
+
+typedef struct s_rect
+{
+	int x;
+	int y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
+
+typedef struct s_data
 {
 	void *mlx_ptr;
+	void *win_ptr;
+	t_img img;
+}	t_data;
 
-	mlx_ptr = mlx_init();
-	mlx_destroy_display(mlx_ptr);
-	free(mlx_ptr);
+int	encode_rgb(unsigned int red, unsigned int green, unsigned int blue)
+{
+	return (red << 16 | green << 8 | blue);
+}
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char    *pixel;
+
+    pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(int *)pixel = color;
+}
+
+int render_rect(t_img *img, t_rect rect)
+{
+	int	i;
+	int j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			img_pix_put(img, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+	if (data->win_ptr == NULL)
+		return (1);
+	render_rect(&data->img, (t_rect){0, 0, 500, 300, RED_PIXEL});
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
+	return (0);
+}
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	printf("Keypress : %d\n", keysym);
+	return(0);
+}
+
+int	main(void)
+{
+	t_data data;
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "MY firrt");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (1);
+	}
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
